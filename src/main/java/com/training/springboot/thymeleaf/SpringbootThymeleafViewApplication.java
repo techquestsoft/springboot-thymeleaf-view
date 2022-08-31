ffd75ab466627bdeade953f4cd084757c28c9c36
yekkuluri <rajasekharareddy@gmail.com>
2022-08-31 21:09:05 +0530

    adding project

---
+++ src/main/java/com/training/springboot/thymeleaf/SpringbootThymeleafViewApplication.java
@@ -0,0 +1,13 @@
+package com.training.springboot.thymeleaf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootThymeleafViewApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootThymeleafViewApplication.class, args);
+	}
+
+}
